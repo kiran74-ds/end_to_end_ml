f3a66f9301f5d95b70ba57b0a55d899d30c977eb
kiran74-ds <meetakoti.kiran@gmail.com>
2020-10-06 08:59:13 +0200

    adding few test cases

--- app/test/test_preprocess.py
+++ app/test/test_preprocess.py
@@ -7,7 +7,9 @@ from pandas.testing import assert_frame_equal
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import GradientBoostingClassifier
-from app.src.preprocess import impute_missing_values, train_model
+from app.src.preprocess import impute_missing_values, train_model, \
+                                normalize_columns, one_hot_coding
+
 
 class TestProcessing(unittest.TestCase):
     ''' Test cases for preprocessing techniques'''
@@ -33,6 +35,24 @@ class TestProcessing(unittest.TestCase):
 
         assert_frame_equal(expected, impute_missing_values(data_frame, ['numeric_column'],
                                                            True), check_dtype=False)
+
+    @staticmethod
+    def test_normalize_column():
+        # pylint: disable=missing-function-docstring
+        mock_frames = pd.DataFrame({'num_columnn': range(10)})
+        normalize_columns(mock_frames, ['num_columnn'])
+        assert abs(mock_frames.num_columnn.mean() - 0) < 0.01
+        assert abs(mock_frames.num_columnn.std() - 1) < 0.01
+
+    @staticmethod
+    def test_one_hot_encoding():
+        # pylint: disable=missing-function-docstring
+        mock_frames = pd.DataFrame({'cat_columnn': ['A','A','B','B','A']})
+
+        expected = pd.DataFrame({'cat_columnn_A' : [1,1,0,0,1],
+                                 'cat_columnn_B' : [0,0,1,1,0],})
+        assert_frame_equal(expected, one_hot_coding(
+            mock_frames, ['cat_columnn']),check_dtype=False)
 
 
     def test_train_model(self):
